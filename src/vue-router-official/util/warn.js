2a6590a07aad3ed5a6b5c52eef52d1e0b3d7e24e
Ray Sun <ray_sun@intsig.net>
2020-10-27 20:17:46 +0800

    add

---
+++ src/vue-router-official/util/warn.js
@@ -0,0 +1,12 @@
+export function assert (condition, message) {
+  if (!condition) {
+    throw new Error(`[vue-router] ${message}`)
+  }
+}
+
+export function warn (condition, message) {
+  if (process.env.NODE_ENV !== 'production' && !condition) {
+    typeof console !== 'undefined' && console.warn(`[vue-router] ${message}`)
+  }
+}
+
